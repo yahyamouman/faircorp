03425dff7b4b699a15fa5c2a33aa3fbd3c071072
yahyamouman <56233662+yahyamouman@users.noreply.github.com>
2022-01-28 12:47:10 +0100

    Update WebConfig.java
    added package imports

--- src/main/java/com/emse/spring/faircorp/WebConfig.java
+++ src/main/java/com/emse/spring/faircorp/WebConfig.java
@@ -1,4 +1,8 @@
 package com.emse.spring.faircorp;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
