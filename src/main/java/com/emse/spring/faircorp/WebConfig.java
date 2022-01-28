438a8da643b53523fff0ec844627b68ea8556a26
yahya <yahyamouman@gmail.com>
2022-01-28 12:35:06 +0100

    Added global config to allow cors

---
+++ src/main/java/com/emse/spring/faircorp/WebConfig.java
@@ -0,0 +1,11 @@
+package com.emse.spring.faircorp;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
